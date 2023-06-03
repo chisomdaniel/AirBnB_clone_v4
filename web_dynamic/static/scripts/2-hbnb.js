599dd474c6c9cfe827f2db60aea220130d5b9e46
Chisom Daniel <chinwezechisom@gmail.com>
2023-06-04 00:09:50 +0100

    fixing scripts

--- web_dynamic/static/scripts/2-hbnb.js
+++ web_dynamic/static/scripts/2-hbnb.js
@@ -1,15 +1,15 @@
+
 $('document').ready(function () {
+
   const url = 'http://' + window.location.hostname + ':5001/api/v1/status/';
   $.get(url, function (reply) {
     if (reply.status === 'OK') {
-      $('DIV#api_status').addClass('available');
+      $('DIV#api_status').addClass('available');    //css('background-color', '#ff545f')
     } else {
       $('DIV#api_status').removeClass('available');
     }
   });
-});
 
-$('document').ready(function () {
   let amenities = {};
   $('INPUT[type="checkbox"]').change(function () {
     if ($(this).is(":checked")) {
